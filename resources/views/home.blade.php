096940b2cf810c7847a184b8a86897c1d0a9459a
Emmanuel Bosquet <bjokac@gmail.com>
2025-07-20 13:33:15 +0200

    create views/layouts/app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,8 @@
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <title>Waiting list</title>
-</head>
+@section('content')
 
-<body>
-    <h1>Waiting list</h1>
+<h1>Waiting list</h1>
 
-    <p class="text-gray-500 bg-amber-200">Nothing to see here, go check out your therapist's page.</p>
-
-</body>
-
-</html>
+<p class="text-gray-500 bg-amber-200">Nothing to see here, go check out your therapist's page.</p>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.css'])
+    <title>@yield('title', 'Waiting list')</title>
+</head>
+
+<body class="min-h-screen bg-gray-50 flex flex-col items-center px-2">
+
+    <main class="w-full max-w-3xl flex-grow">
+        @yield('content')
+    </main>
+
+    <footer class="w-full max-w-3xl mt-6 text-center">
+        @yield('footer')
+    </footer>
+</body>
